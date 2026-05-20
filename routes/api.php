dfaed982c52920143573d3b62831e59ef98fc84b
RangelDheo <rangeldheo@gmail.com>
2026-05-20 18:12:21 -0300

    feat(Product): create route Purchase

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,18 @@
 <?php
 
 use App\Http\Controllers\Api\ProductController;
+use App\Http\Controllers\Api\PurchaseController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('produtos')->group(function () {
     Route::get('/', [ProductController::class, 'index']);
 
     Route::post('/', [ProductController::class, 'store']);
+});
+
+Route::prefix('compras')->group(function () {
+    Route::post('/', [
+        PurchaseController::class,
+        'store',
+    ]);
 });
